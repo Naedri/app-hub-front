eccfa9b664bcc936a9e3775fe935b2a722e8abfb
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-06-28 17:02:07 +0200

    build: update config
    tsconfig : target e5 => e6

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ms-users-front',
+  appId: 'com.ms-users.app',
+  appName: 'ms-users',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
