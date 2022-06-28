2f0a9cfd22a3aa6c08a6e353afa604669331b8dd
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-06-28 18:22:20 +0200

    test: setupTest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,11 +4,35 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 
-// Mock matchmedia
-window.matchMedia = window.matchMedia || function() {
+// react-testing-library renders your components to document.body,
+// this adds jest-dom's custom assertions
+import '@testing-library/jest-dom';
+
+const queries = {
+  small: '(max-width: 599px)',
+  medium: '(min-width: 600px) and (max-width: 1023px)',
+  large: '(min-width: 1024px)',
+  screen: 'only screen',
+};
+
+const matchMedia = (match: any) => (query: string) => {
   return {
-      matches: false,
-      addListener: function() {},
-      removeListener: function() {}
+    matches: query === match || query === queries.screen,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
   };
 };
+
+describe('<MediaManager />', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('window.matchMedia', () => {
+    test('setup - event listeners and initial state', () => {
+      window.matchMedia = jest.fn().mockImplementation(matchMedia(queries['small']));
+    });
+  });
+});
