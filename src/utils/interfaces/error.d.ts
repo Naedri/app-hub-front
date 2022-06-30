4535868ac2a120aa3a18ee6e35a3946ba2d648bc
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-06-30 15:31:00 +0200

    feat: ErrorClient and ErrorFromServer

--- src/utils/interfaces/error.d.ts
+++ src/utils/interfaces/error.d.ts
@@ -1,6 +1,11 @@
-export type ErrorClient = {
+export type ErrorFromServer = {
   response: {
     status: number;
   };
   message: string;
 };
+
+export interface ErrorClient {
+  name: string;
+  message: string;
+}
