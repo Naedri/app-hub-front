bb50d5d40fcb4d166ea0e93df0a531e1c9b41c7e
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-07-08 14:31:48 +0200

    refactor: more specific type for app description

--- src/utils/interfaces/iapp.d.ts
+++ src/utils/interfaces/iapp.d.ts
@@ -1,8 +1,8 @@
-//TODO update description type
+// IApp.description is optional, but if it is defined it must include at least 'en' attribute
 export interface IApp {
   id: number;
   name: string;
   landingPage?: string;
-  description?: { en: string; no: string };
+  description?: { en: string; [key?: string]: string };
   url?: string;
 }
