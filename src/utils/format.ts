6503a4820acada0204b5d3a45ea6cd2fe46f2e6f
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-07-18 15:07:03 +0200

    feat: string to kebab or snake functions

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,13 +1,53 @@
 import type { AxiosError } from 'axios';
 
+import type { Page } from '../types/enums/pages';
 import type { ErrorFromServer } from '../types/interfaces/error';
 
 /**
  * CASE
+ * https://en.wikipedia.org/wiki/Naming_convention_(programming)#Examples_of_multiple-word_identifier_formats
  */
 
+/**
+ *
+ * @example notFound => NotFound
+ */
 export function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function uncapitalizeFirstLetter(string: string): string {
+  return string.charAt(0).toLowerCase() + string.slice(1);
+}
+
+/**
+ *
+ * @example notFound or NotFound => not_found
+ */
+export function pascalToSnake(str: string): string {
+  return str
+    .split(/(?=[A-Z])/)
+    .join('_')
+    .toLowerCase();
+}
+
+/**
+ *
+ * @example notFound or NotFound => not-found
+ */
+export function pascalToKebab(str: string): string {
+  return str
+    .split(/(?=[A-Z])/)
+    .join('-')
+    .toLowerCase();
+}
+
+/**
+ * I18
+ */
+
+export function mapPageToTitle(page: Page): string {
+  return `${uncapitalizeFirstLetter(page)}PageTitle`;
 }
 
 /**
